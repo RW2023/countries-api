30f09bcd8cd85c21df7d692f654af9b53d568f45
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-04 18:25:18 -0400

    remove custom color definitions from Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,19 +11,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        overlay: 'rgba(43, 45, 66, 0.5)',
-        background: '#2b2d42',
-        button: '#f8f32b',
-        headline: '#ffffff',
-        paragraph: '#8d99ae',
-        buttonText: '#2b2d42',
-        stroke: '#8d99ae',
-        main: '#f8f32b',
-        highlight: '#f8f32b',
-        secondary: '#8d99ae',
-        tertiary: '#000000',
-      },
       fontFamily: {
         sans: ['Poppins', 'sans'],
         serif: ['Roboto Slab', 'serif'],
