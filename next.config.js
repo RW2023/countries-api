f4e24ac35afb45d291107d3753a2f3cb2fa6833c
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-05 04:25:42 -0400

    feat: configure image domains in next.config.js for external image support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['flagcdn.com', 'upload.wikimedia.org'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
